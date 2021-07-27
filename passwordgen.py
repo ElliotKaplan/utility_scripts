188cac20df4323d4be86b2c969cb94f77085661f
Elliot Kaplan <elliot.kaplan@gmail.com>
2021-07-27 16:07:19 -0400

    allow passwords w/o special characters

--- passwordgen.py
+++ passwordgen.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 from random import choice
+from string import ascii_uppercase, ascii_lowercase, digits
 
 from argparse import ArgumentParser
+
 
 argparser = ArgumentParser('generates a random password')
 argparser.add_argument('-n', '--nchars', type=int, default=16,
@@ -11,10 +13,16 @@ argparser.add_argument('-r', '--range', nargs=2, default=(0x21, 0x80),
                        help='range of possible values')
 argparser.add_argument('-d', '--digits', action='store_true',
                        help='only output digits')
+argparser.add_argument('--no_special', action='store_true',
+                       help='set to not include special characters')
 
 clargs = argparser.parse_args()
 if clargs.digits:
     clargs.range = (0x30, 0x39)
+if clargs.no_special:
+    chrs = ascii_lowercase + ascii_uppercase + digits
+else:
+    chrs = list(map(chr, range(*map(int, clargs.range))))
 
-chrs = list(map(chr, range(*map(int, clargs.range))))
+
 print(''.join(choice(chrs) for _ in range(clargs.nchars)))
