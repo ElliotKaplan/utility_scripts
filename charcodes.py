4b951ffac17db6c420bcb1a213895574249402be
Elliot Kaplan <elliot.kaplan@gmail.com>
2022-11-10 10:34:52 -0500

    added octal support

--- charcodes.py
+++ charcodes.py
@@ -3,5 +3,15 @@
 from itertools import chain
 import sys
 
+if sys.argv[1].startswith('-o'):
+    for char in chain(*sys.argv[2:]):
+        print(char, "\t", oct(ord(char)))
+    sys.exit()
+    
+if sys.argv[-1].startswith('-o'):
+    for char in chain(*sys.argv[1:-1]):
+        print(char, "\t", oct(ord(char)))
+    sys.exit()
+
 for char in chain(*sys.argv[1:]):
     print(char, "\t", hex(ord(char)))
