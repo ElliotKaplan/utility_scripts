f6d53a1e3c997e6062aa6f47cb98c2f967287e54
elliot <elliot.kaplan@gmail.com>
2025-10-30 14:13:06 -0400

    set operation utility

---
+++ setops.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def setops(a_file, b_file, op='union', sep=None):
+    a = set(a_file.read().split(sep))
+    b = set(b_file.read().split(sep))
+
+    return getattr(a, op)(b)
+    
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser, FileType
+    import sys
+
+    argparser = ArgumentParser('performs set operations on files')
+    argparser.add_argument('operation', type=str, choices=['union', 'intersection', 'difference', 'symmetric_difference'],
+                           help='set operation to perform')
+    argparser.add_argument('set_a', type=FileType('r'), help='first set')
+    argparser.add_argument('set_b', type=FileType('r'), default=sys.stdin,
+                           help='second set, can be read from stdin')
+
+    clargs = argparser.parse_args()
+    print('\n'.join(setops(clargs.set_a, clargs.set_b, clargs.operation)))
